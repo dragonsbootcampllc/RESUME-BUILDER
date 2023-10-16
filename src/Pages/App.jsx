37acdc9ce13d553f695439d4eac3e12e58912cc1
Faisal Ibrahim <79205676+DarkCode662432@users.noreply.github.com>
2023-10-16 19:38:13 +0300

    edit src of files

---
+++ src/Pages/App.jsx
@@ -0,0 +1,21 @@
+// import PersonalInfoForm from './Pages/Component/PersonalInfoForm';
+// import SocialLinks from './Pages/Component/SocialLinks';
+// import EmploymentHistory from './Pages/Component/EmploymentHistory';
+// import CVTemplate from './Pages/Component/CVTemplate';
+// import Container from './Pages/Component/Container';
+import HobbiesSection from "./Component/HobbiesSection";
+
+function App() {
+  return (
+    <>
+      {/* <PersonalInfoForm /> */}
+      {/* <SocialLinks /> */}
+      {/* <EmploymentHistory /> */}
+      {/* <CVTemplate /> */}
+      {/* <Container /> */}
+      <HobbiesSection />
+    </>
+  )
+}
+
+export default App;
