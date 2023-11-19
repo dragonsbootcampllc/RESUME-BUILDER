53e94eb864353c1907bf640323633a2c53926d0e
hamdysaad20 <hamdysaad.biz@gmail.com>
2023-11-19 14:37:39 +0200

    PDF Fix Responsive and full screen

--- src/Pages/Component/CVTemplate.jsx
+++ src/Pages/Component/CVTemplate.jsx
@@ -22,7 +22,7 @@ function CVTemplate() {
   const { employmentHistory } = useEmploymentHistory();
 
   return (
-    <section style={{ width: `100%` }} className='bg-white max-w-[21cm] h-[27.8cm] p-[50px] box-border break-words '>
+    <section style={{ width: `100%` }} className='bg-white max-w-[21cm] h-[27.8cm]  p-[50px] box-content break-words   '>
       <div className=' flex  gap-20'>
         <div className="w-[150px] h-[150px] box-border rounded-full flex justify-center items-center overflow-hidden">
           <img src={userImageUrl} className="w-full" draggable="false"/>
@@ -35,7 +35,7 @@ function CVTemplate() {
 
       <div className=' flex gap-[15%] mt-[40px]'>
         <div className=' grid gap-[20px] w-[40%] xd:block'>
-          <p className=' font-bold pb-[20px] border-b-[1px] border-sky-600 text-[30px] capitalize'>education</p>
+          <p className=' font-bold pb-[20px] border-b-[1px] border-sky-600 box-content text-[30px] capitalize'>education</p>
 
           <div>
             <p >2014 - 2020</p>
@@ -55,8 +55,8 @@ function CVTemplate() {
             <p className=' text-slate-600 '>university name here</p>
           </div>
 
-          <div>
-            <p className='font-bold pb-[20px] border-b-[1px] border-sky-600 text-[30px] capitalize'>contact</p>
+          <div className="relative box-content max-w-[200px]">
+            <p className='font-bold pb-[20px] border-b-[1px] border-sky-600 relative box-content max-w-[200px] text-[30px] capitalize'>contact</p>
             <div className='flex flex-col gap-2 mt-2 text-lg'>
               <a className='text-blue-700 underline' href={`tel:${userPhone}`} >{userPhone}</a>
               <a className='text-blue-700 underline' href={`mailto:${userEmail}`} >{userEmail}</a>
@@ -68,15 +68,15 @@ function CVTemplate() {
           </div>
         </div>
 
-        <div>
-          <p className=' font-bold pb-[20px] border-b-[1px] border-sky-600 mb-[15px] text-[30px] capitalize' > profile </p>
+        <div className="box-content break-words p-2  relative mr-1 w-full h-full">
+          <p className=' font-bold pb-[20px] border-b-[1px] border-sky-600 mb-[15px] box-content max-w-[60%] text-[30px] capitalize' > profile </p>
 
-          <p className=' mb-[30px]'> {userAbout} </p>
+          <p className=' mb-[30px] break-words  max-w-[440px] mr-1'> {userAbout} </p>
 
-          <p className='  font-bold pb-[20px] border-b-[1px] border-sky-600 text-[30px] mb-[55px] capitalize' >experience</p>
+          <p className='  font-bold pb-[20px] border-b-[1px] border-sky-600 text-[30px] box-content max-w-[60%] mb-[55px] capitalize' >experience</p>
 
           <div className='flex gap-[40px]'>
-            <div className='grid gap-[55px] xd:block'>
+            <div className='grid gap-[55px] xd:block box-content break-words max-w-[400px] mr-1 ' >
               {employmentHistory.map((ele, idx) => (
                 <div key={idx}>
                   <p className=' text-slate-600 '>{ele.startDate} - {ele.endDate}</p>
@@ -85,13 +85,13 @@ function CVTemplate() {
               ))}
             </div>
 
-            <div className='grid xd:block '>
+            <div className='grid xd:block   box-content break-words  max-w-[400px] mr-1'>
               <div>
               </div>
               {employmentHistory.map((ele, idx) => (
                 <div key={idx}>
-                  <p className=' font-bold text-[16px] '>{ele.jobTitle}</p>
-                  <p className=' text-slate-600 '>{ele.details}</p>
+                  <p className=' font-bold text-[16px]  break-words p-2 max-w-[400px] mr-1  '>{ele.jobTitle}</p>
+                  <p className=' text-slate-600 break-words p-2 pr-20 max-w-[400px] mr-1'>{ele.details}</p>
                 </div>
               ))}
             </div>
